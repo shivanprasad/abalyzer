d4ffa8c3f52ef8f16a86606780538f583bba7f03
Shivan Prasad <68702232+shivanprasad@users.noreply.github.com>
2022-03-14 15:36:19 -0400

    feat: implemented is_aba
    is_aba now returns True or False depending if the passed in number is a valid ABA bank number

--- abalyzer/parser/__main__.py
+++ abalyzer/parser/__main__.py
@@ -1,3 +1,5 @@
+import array
+
 
 # Parses an ACH number and returns information about it
 def parse_aba(bank_number):
@@ -11,9 +13,25 @@ def parse_ach(bank_number):
 
 # Parses a user-defined number to see if it is an ABA number
 def is_aba(bank_number):
-    return True
+    output = False
+    bank_number = str(bank_number)
+    chars = [int(i) for i in bank_number]
+    length = len(bank_number)
+    print(bank_number)
+
+    if length == 9:
+        if ((3 * (chars[0] + chars[3] + chars[6]) +
+             7 * (chars[1] + chars[4] + chars[7]) +
+             (chars[2] + chars[5] + chars[8]))) % 10 == 0:
+            output = True
+    else:
+        output = False
+    return output
 
 
 # Parses a user-defined number to see if it is an ACH number
 def is_ach(bank_number):
     return True
+
+
+print(is_aba(111000025))
