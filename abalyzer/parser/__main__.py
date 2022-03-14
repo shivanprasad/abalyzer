766e48bf2e039c8555feecaf74e5003093763ca4
Shivan Prasad <68702232+shivanprasad@users.noreply.github.com>
2022-03-14 14:28:31 -0400

    add: parser method

---
+++ abalyzer/parser/__main__.py
@@ -0,0 +1,19 @@
+
+# Parses an ACH number and returns information about it
+def parse_aba(bank_number):
+    return 0
+
+
+# Parses an ACH number and returns information about it
+def parse_ach(bank_number):
+    return 0
+
+
+# Parses a user-defined number to see if it is an ABA number
+def is_aba(bank_number):
+    return True
+
+
+# Parses a user-defined number to see if it is an ACH number
+def is_ach(bank_number):
+    return True
